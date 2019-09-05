cb0d971418872545e94a9c27d75a8c345171e495
hongyanyang1 <hongyanyang1@creditease.cn>
2019-09-05 10:59:20 +0800

    GPU = 1

--- bin/train.sh
+++ bin/train.sh
@@ -11,7 +11,7 @@ fi
 if [ "$1" = "console" ]; then
     echo "调试模式"
     # 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
-    CUDA_VISIBLE_DEVICES=0 \
+    CUDA_VISIBLE_DEVICES=1 \
     python \
         -m tools.train \
         --data_dir=data/ \
