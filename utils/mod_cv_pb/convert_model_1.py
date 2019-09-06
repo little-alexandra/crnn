a2e59284ba8e19b50b33abc90ac4007646c83b61
hongyanyang1 <hongyanyang1@creditease.cn>
2019-09-06 11:22:40 +0800

    增加转换模型脚本

--- utils/mod_cv_pb/convert_model_1.py
+++ utils/mod_cv_pb/convert_model_1.py
@@ -10,24 +10,30 @@ from config import config
 from crnn_model import crnn_model
 from local_utils import data_utils
 
-tf.app.flags.DEFINE_boolean('debug', False, '')
-tf.app.flags.DEFINE_float('learning_rate', 0.001, '')
+tf.app.flags.DEFINE_boolean('debug', True, '')
+tf.app.flags.DEFINE_string('ckpt_mod_path', "", '')
+tf.app.flags.DEFINE_string('charset', "../../charset.3770.txt", '')
+tf.app.flags.DEFINE_string('save_mod_dir', "./model/crnn", '')
+
+FLAGS = tf.app.flags.FLAGS
 
 
 def convert():
     # 保存转换好的模型目录
-    savedModelDir = "./models/crnn"
+    saveModDir = FLAGS.save_mod_dir
     # 每次转换都生成一个版本目录
     for i in range(100000, 9999999):
-        cur = os.path.join(savedModelDir, str(i))
+        cur = os.path.join(saveModDir, str(i))
         if not tf.gfile.Exists(cur):
-            savedModelDir = cur
+            saveModDir = cur
             break
+    print("模型保存目录", saveModDir)
     # 原ckpt模型
-    ckptModPath = "../../../../models_ckpt/crnn_2019-06-27-05-06-48.ckpt-58000"
-    ckptModPath = "../../model/crnn_2019-09-04-18-57-44.ckpt-4"
+    ckptModPath = FLAGS.ckpt_mod_path
+    print("CKPT模型目录", ckptModPath)
+    print("charset目录", FLAGS.charset)
     # 获取字符库
-    charset = data_utils.get_charset("../../charset.3770.txt")
+    charset = data_utils.get_charset(FLAGS.charset)
 
     # 定义张量
     input_image = tf.placeholder(tf.float32, shape=[None, 32, None, 3], name='input_image')
@@ -42,13 +48,12 @@ def convert():
         net_out = network.build(inputdata=input_image, sequence_len=sequence_size)
     # 创建校验用的decode和编辑距离
     validate_decode, shape, indices, values = network.validate(net_out, sequence_size)
-    print(validate_decode)
     saver = tf.train.Saver()
     session = tf.Session()
     saver.restore(sess=session, save_path=ckptModPath)
 
     # 保存转换训练好的模型
-    builder = tf.saved_model.builder.SavedModelBuilder(savedModelDir)
+    builder = tf.saved_model.builder.SavedModelBuilder(saveModDir)
     inputs = {
         "input_data": tf.saved_model.utils.build_tensor_info(input_image),
         "input_batch_size": tf.saved_model.utils.build_tensor_info(sequence_size),
@@ -71,6 +76,7 @@ def convert():
         }
     )
     builder.save()
+    print("转换模型结束", saveModDir)
 
 
 if __name__ == '__main__':
