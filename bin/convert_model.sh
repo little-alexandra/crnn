dd1ca8c6802980037a11312919a037934faa91c2
piginzoo <piginzoo@gmail.com>
2019-09-17 19:06:11 +0800

    bugfix

--- bin/convert_model.sh
+++ bin/convert_model.sh
@@ -2,13 +2,18 @@
 
 echo "模型转换: 从ckpt转成pb"
 
-ckpt_mod_path=$1
-save_mod_dir=$2
-
-if [ "$ckpt_mod_path" = "" ]; then
+if [ "$1" = "" ]; then
     echo "ckpt模型目录不能为空: conver_model <原始模型的名称> <目标pb模型的生成目录>"
     echo "例："
     echo "  bin/convert_model.sh /app.fast/projects/models/crnn_2019-06-27-05-06-48.ckpt-58000  ../ocr/model/crnn"
+fi
+
+ckpt_mod_path=$1
+save_mod_dir=$2
+
+if [ "$1" = "quick" ]; then
+    ckpt_mod_path=/app.fast/projects/models/crnn_2019-06-27-05-06-48.ckpt-58000
+    save_mod_dir=../ocr/model/crnn
 fi
 
 if [ "$save_mod_dir" = "" ]; then
